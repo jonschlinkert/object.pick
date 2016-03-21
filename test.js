4a44129f0f98fa09e5fcd285f99368f8b366530e
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-21 04:36:12 -0400

    adds unit tests
    adds vinyl file tests based on usage in gulp-cache, other tests based on usage IBM watson and other libs

--- test.js
+++ test.js
@@ -7,21 +7,48 @@
 
 'use strict';
 
-var should = require('should');
+require('mocha');
+var assert = require('assert');
+var File = require('vinyl');
 var pick = require('./');
 
 describe('.pick()', function() {
-  it('should pick a property from an object.', function() {
-    pick({a: 'a', b: 'b'}, 'a').should.eql({a: 'a'});
+  it('should return an empty object when the value is falsey', function() {
+    assert.deepEqual(pick(null, 'a'), {});
+    assert.deepEqual(pick(false, 'a'), {});
+    assert.deepEqual(pick('', 'a'), {});
   });
 
-  it('should pick keys specified as arrays.', function() {
-    pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b']).should.eql({a: 'a', b: 'b'});
-    pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar']).should.eql({foo: 'foo', bar: 'bar'});
+  it('should pick a property from an object', function() {
+    assert.deepEqual(pick({a: 'a', b: 'b'}, 'a'), {a: 'a'});
   });
 
-  it('should ignore keys that don\'t exist.', function() {
-    pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b', 'foo']).should.eql({a: 'a', b: 'b'});
-    pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar', 'abc']).should.eql({foo: 'foo', bar: 'bar'});
+  it('should pick getter properties', function() {
+    var file = new File({path: __filename, contents: new Buffer('foo')});
+    var obj = pick(file, ['cwd', 'relative', 'base', 'contents', 'stat', 'history']);
+    assert(obj.hasOwnProperty('cwd'));
+    assert(obj.hasOwnProperty('base'));
+    assert(obj.hasOwnProperty('relative'));
+    assert(obj.hasOwnProperty('contents'));
+    assert(obj.hasOwnProperty('stat'));
+    assert(obj.hasOwnProperty('history'));
+    assert.equal(obj.relative, 'test.js');
+  });
+
+  it('should pick a property from a function', function() {
+    function fn() {}
+    fn.a = 'foo';
+    fn.b = 'bar';
+    assert.deepEqual(pick(fn, 'a'), {a: 'foo'});
+  });
+
+  it('should pick multiple properties', function() {
+    assert.deepEqual(pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b']), {a: 'a', b: 'b'});
+    assert.deepEqual(pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar']), {foo: 'foo', bar: 'bar'});
+  });
+
+  it('should ignore keys that do not exist', function() {
+    assert.deepEqual(pick({a: 'a', b: 'b', c: 'c'}, ['a', 'b', 'foo']), {a: 'a', b: 'b'});
+    assert.deepEqual(pick({foo: 'foo', bar: 'bar', baz: 'baz'}, ['foo', 'bar', 'abc']), {foo: 'foo', bar: 'bar'});
   });
 });

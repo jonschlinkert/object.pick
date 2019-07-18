2492229285626a321dc9d74ef10d044be80df130
刘放 <362512489@qq.com>
2019-07-18 10:55:02 +0800

    feat: support alias to transform value of key

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ module.exports = function pick(obj, keys) {
   if (!isObject(obj) && typeof obj !== 'function') {
     return {};
   }
+  var newKey;
+  var aliasKeys;
 
   var res = {};
   if (typeof keys === 'string') {
+    aliasKeys = transAliasKey(keys);
+    newKey = aliasKeys.newKeys;
+    keys = aliasKeys.keys;
     if (keys in obj) {
-      res[keys] = obj[keys];
+      res[newKey] = obj[keys];
     }
     return res;
   }
@@ -27,9 +32,32 @@ module.exports = function pick(obj, keys) {
 
   while (++idx < len) {
     var key = keys[idx];
+    aliasKeys = transAliasKey(key);
+    newKey = aliasKeys.newKeys;
+    key = aliasKeys.keys;
     if (key in obj) {
-      res[key] = obj[key];
+      res[newKey] = obj[key];
     }
   }
   return res;
 };
+
+/**
+ * Get real key from alias key
+ * @param {String|Array} keys keys or alias to pick
+ * @returns {Object} obj
+ * @returns {String} obj.newKeys target key as alias or not
+ * @returns {String} obj.keys source key to pick
+ */
+function transAliasKey (keys) {
+  var alias = keys.trim().split(/\s+as\s+/);
+  var newKeys = keys;
+  if (alias.length == 2) {
+    newKeys = alias[1];
+    keys = alias[0];
+  }
+  return {
+    newKeys: newKeys,
+    keys: keys
+  }
+}
